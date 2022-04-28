e1f66957f955ccf3003d1bcfe1487bd1014e8fe7
MichaelWu <vmichael.wu@gmail.com>
2022-04-28 14:21:46 -0700

    feat: working css loader

---
+++ client/src/components/AddForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddForm = () => {
+  return <div>Add form goes here</div>;
+};
+
+export default AddForm;
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import Header from '../components/Header.jsx';
+import AddForm from '../components/AddForm.jsx';
+import List from '../components/List.jsx';
+
 const App = () => {
-  return <div>hello world! This is my first App!</div>;
+  return (
+    <div>
+      App begins here
+      <div id='testing'>Testing</div>
+      <Header />
+      <AddForm />
+      <List />
+    </div>
+  );
 };
 
 export default App;
